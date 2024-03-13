585be8222c41afab45a85e62db4a8ba82c2d9d27
Shubham-Pawar-99 <159262602+Shubham-Pawar-99@users.noreply.github.com>
2024-03-13 14:23:07 +0530

    Change class based component to function based component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React, { Component } from 'react';
+import React, {  useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './component/Navbar';
 import News from './component/News';
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-  pageSize = 5;
-  apikey='b7b9c331bf0d4b31bb3e15998a9eab58';
+const App = () => {
+  const pageSize = 5;
+  const apikey = 'ccf3deef75664279b22b10ef226d6fe4';
+  const [progress, setProgress] = useState(0);
 
-  state = {
-    progress: 0
-  }
-  setProgress = (progress) => {
-    this.setState({ progress: progress })
-  }
-
-  render() {
-
-    return (
-      <Router>
-        <div>
-          <Navbar />
-          <LoadingBar color='#f11946' height={3} progress={this.state.progress} />
-          <Routes>
-            <Route exact path="/" element={<News setProgress={this.setProgress} apikey={this.apikey} key="general" pageSize={this.pageSize} category="general" />} />
-            <Route exact path="/technology" element={<News setProgress={this.setProgress} apikey={this.apikey} key="technology" pageSize={this.pageSize} category="technology" />} />
-            <Route exact path="/business" element={<News setProgress={this.setProgress} apikey={this.apikey} key="business" pageSize={this.pageSize} category="business" />} />
-            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apikey={this.apikey} key="entertainment" pageSize={this.pageSize} category="entertainment" />} />
-            <Route exact path="/general" element={<News setProgress={this.setProgress} apikey={this.apikey} key="general" pageSize={this.pageSize} category="general" />} />
-            <Route exact path="/health" element={<News setProgress={this.setProgress} apikey={this.apikey} key="health" pageSize={this.pageSize} category="health" />} />
-            <Route exact path="/science" element={<News setProgress={this.setProgress} apikey={this.apikey} key="science" pageSize={this.pageSize} category="science" />} />
-            <Route exact path="/sports" element={<News setProgress={this.setProgress} apikey={this.apikey} key="sports" pageSize={this.pageSize} category="sports" />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <LoadingBar color='#f11946' height={3} progress={progress} />
+        <Routes>
+          <Route exact path="/" element={<News setProgress={setProgress} apikey={apikey} key="general" pageSize={pageSize} category="general" />} />
+          <Route exact path="/technology" element={<News setProgress={setProgress} apikey={apikey} key="technology" pageSize={pageSize} category="technology" />} />
+          <Route exact path="/business" element={<News setProgress={setProgress} apikey={apikey} key="business" pageSize={pageSize} category="business" />} />
+          <Route exact path="/entertainment" element={<News setProgress={setProgress} apikey={apikey} key="entertainment" pageSize={pageSize} category="entertainment" />} />
+          <Route exact path="/general" element={<News setProgress={setProgress} apikey={apikey} key="general" pageSize={pageSize} category="general" />} />
+          <Route exact path="/health" element={<News setProgress={setProgress} apikey={apikey} key="health" pageSize={pageSize} category="health" />} />
+          <Route exact path="/science" element={<News setProgress={setProgress} apikey={apikey} key="science" pageSize={pageSize} category="science" />} />
+          <Route exact path="/sports" element={<News setProgress={setProgress} apikey={apikey} key="sports" pageSize={pageSize} category="sports" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
+export default App;
--- src/component/News.js
+++ src/component/News.js
@@ -1,130 +1,122 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import InfiniteScroll from "react-infinite-scroll-component";
 import Spinner from './Spinner';
 import NewsItem from './NewsItem';
 
-export default class News extends Component {
-  static defaultProps = {
-    country: 'in',
-    pageSize: 8,
-    category: 'general',
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-      searchTerm: '',
-    };
-    document.title = `${this.capitalize(this.props.category)}-Daily News`;
-  }
-
-  capitalize = (str) => {
+  const capitalize = (str) => {
     return str && typeof str === 'string' ? str.charAt(0).toUpperCase() + str.slice(1) : str;
   };
-  
-  async updateNews(props) {
-    const { country, category, pageSize,apikey } = this.props;
-    const { page, searchTerm } = this.state;
 
-    this.props.setProgress(10);
+  const updateNews = async () => {
+    const { country, category, pageSize, apikey, setProgress } = props;
+    setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apikey}&page=${page}&pageSize=${pageSize}&q=${searchTerm}`;
-    this.props.setProgress(20);
+    setProgress(20);
     try {
       const response = await fetch(url);
       const parseData = await response.json();
-      this.props.setProgress(30);
-
-      this.setState((prevState) => ({
-        articles: page === 1 ? parseData.articles : [...prevState.articles, ...parseData.articles],
-        totalResults: parseData.totalResults,
-       
-        loading: false,
-      }));
-      this.props.setProgress(70);
+      setProgress(30);
+      setArticles((prevArticles) => (page === 1 ? parseData.articles : [...prevArticles, ...parseData.articles]));
+      setTotalResults(parseData.totalResults);
+      setLoading(false);
+      setProgress(70);
     } catch (error) {
       console.error('Error fetching news:', error);
-      this.setState({ loading: false });
+      setLoading(false);
     }
-    this.props.setProgress(100);
+    setProgress(100);
   };
 
-  componentDidMount() {
-    this.updateNews();
-  }
+  useEffect(() => {
+    document.title = `${capitalize(props.category)} - Daily News`;
+    updateNews();
+    //eslint-disable-next-line
+  }, [page]);
 
-  handleSearchChange = (event) => {
-    this.setState({ searchTerm: event.target.value });
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
   };
 
-  handleSearchSubmit = (event) => {
+  const handleSearchSubmit = (event) => {
     event.preventDefault();
-    this.setState({ page: 1 }, this.updateNews);
+    setPage(1);
+    updateNews();
   };
 
-  fetchMoreData = () => {
-    this.setState((prevState) => ({ page: prevState.page + 1 }), this.updateNews);
+  const fetchMoreData = () => {
+    setPage((prevPage) => prevPage + 1);
   };
 
-  render() {
-    const { articles, totalResults, loading, searchTerm } = this.state;
-    const { category } = this.props;
+  const { category } = props;
 
-    return (
-      <InfiniteScroll
-        dataLength={articles.length}
-        next={this.fetchMoreData}
-        hasMore={articles.length !== totalResults}
-        loader={<Spinner className="my-3" />}
-      >
-        <div className="container">
-          <form className="d-flex my-2" style={{ float: 'right', width: '300px' }} onSubmit={this.handleSearchSubmit}>
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search News..."
-              aria-label="Search"
-              value={searchTerm}
-              onChange={this.handleSearchChange}
-            />
-            <button className="btn btn-outline-dark" type="submit">
-              Search
-            </button>
-          </form>
-          <h1 className="my-3">DailyNews - Top {this.capitalize(category)} Headlines</h1>
-          {articles.length === 0 && !loading && (
-            <div className="alert alert-warning" role="alert">
-              No news articles found for the search term {searchTerm} in the category {this.capitalize(category)}.
-            </div>
-          )}
-          <div className="row">
-            {articles.map((element) => (
-              <div className="col-md-4 my-3" key={element.url}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imageUrl={element.urlToImage || 'https://c8.alamy.com/comp/CN6EWM/daily-news-newspaper-roll-with-white-background-CN6EWM.jpg'}
-                  url={element.url}
-                  date={element.publishedAt}
-                  author={element.author}
-                  source={element.source.name}
-                />
-              </div>
-            ))}
+  return (
+    <InfiniteScroll
+      dataLength={articles.length}
+      next={fetchMoreData}
+      hasMore={articles.length !== totalResults}
+      loader={<Spinner className="my-3" />}
+      style={{marginTop:'50px'}}
+    >
+      <div className="container my-3" >
+        <form className="d-flex my-2" style={{ float: 'right', width: '300px' }} onSubmit={handleSearchSubmit}>
+          <input id="search"
+            className="form-control me-2"
+            type="search"
+            placeholder="Search News..."
+            aria-label="Search"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button className="btn btn-outline-dark" type="submit">
+            Search
+          </button>
+        </form>
+        <h1 className="my-3">DailyNews - Top {capitalize(category)} Headlines</h1>
+        {articles.length === 0 && !loading && (
+          <div className="alert alert-warning" role="alert">
+            No news articles found for the search term {searchTerm} in the category {capitalize(category)}.
           </div>
+        )}
+        <div className="row">
+          {articles.map((element) => (
+            <div className="col-md-4 my-3" key={element.url}>
+              <NewsItem
+                title={element.title}
+                description={element.description}
+                imageUrl={element.urlToImage || 'https://c8.alamy.com/comp/CN6EWM/daily-news-newspaper-roll-with-white-background-CN6EWM.jpg'}
+                url={element.url}
+                date={element.publishedAt}
+                author={element.author}
+                source={element.source.name}
+              />
+            </div>
+          ))}
         </div>
-      </InfiniteScroll>
-    );
-  }
+      </div>
+    </InfiniteScroll>
+  );
+};
 
-}
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: 'general',
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+  apikey: PropTypes.string,
+  setProgress: PropTypes.func,
+};
+
+export default News;
